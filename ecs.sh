6536291dc61939143a036e41cf1836e828fa0a80
Pamidu jayaranga <pamidu@users.noreply.github.com>
2019-10-26 09:52:46 +0530

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -22,12 +22,14 @@ if [ $DESIRED_COUNT = 0 ]; then
    DESIRED_COUNT=1
 fi
 
-aws ecs register-task-definition --family ${TASK_FAMILY} --network-mode awsvpc --requires-compatibilities FARGATE --execution-role-arn arn:aws:iam::${aws_account}:role/ecsTaskExecutionRole --cpu 256 --memory 512 --container-definitions "name=${SERVICE_NAME},image=$aws_account.dkr.ecr.$aws_region.amazonaws.com/learn:apache_v_${BUILD_NUMBER},portMappings=[{containerPort=80}]"
+aws ecs register-task-definition --family ${TASK_FAMILY} --network-mode awsvpc --requires-compatibilities FARGATE --execution-role-arn arn:aws:iam::229004046319:role/ecsTaskExecutionRole --cpu 256 --memory 512 --container-definitions "name=apache,image=$aws_account.dkr.ecr.$aws_region.amazonaws.com/learn:apache_v_${BUILD_NUMBER},portMappings=[{containerPort=80}]" > taskdef.json
+REVISON_NUMBER=$(jq '.taskDefinition.revision' taskdef.json)
 # aws ecs update-service --cluster SI-Dev-Cluster --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
 OLDER_TASK=$(aws ecs list-tasks --cluster PRS-DEV-CLUSTER --desired-status RUNNING --family ${TASK_FAMILY} | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/")
 echo "Older Task running  " + $OLDER_TASK
 aws ecs stop-task --cluster PRS-DEV-CLUSTER --task ${OLDER_TASK}
 #aws ecs update-service --cluster PRS-DEV-CLUSTER --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count "1"
 
-aws ecs update-service --cluster "PRS-DEV-CLUSTER" --service "${SERVICE_NAME}" --task-definition "${TASK_FAMILY}":"${BUILD_NUMBER}" --desired-count 1 --force-new-deployment
+aws ecs update-service --cluster "PRS-DEV-CLUSTER" --service "${SERVICE_NAME}" --task-definition "${TASK_FAMILY}":"${REVISON_NUMBER}" --desired-count 1 --force-new-deployment
 #aws ecs update-service --cluster "PRS-DEV-CLUSTER" --service "apache" --task-definition "learn":"1" --desired-count 1 --force-new-deployment
+rm taskdef.jsom
